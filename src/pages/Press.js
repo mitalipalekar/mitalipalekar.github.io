c09da5092179bc30df2c4d2aa73a5b8ea1287941
Mitali Palekar <mitali97@cs.washington.edu>
2018-08-16 23:37:37 -0700

    Adding new build directory to root

--- src/pages/Press.js
+++ src/pages/Press.js
@@ -18,7 +18,7 @@ export default class Press extends React.Component {
 	      	<li><a href='http://q13fox.com/2017/05/12/with-unique-scholarship-program-seattle-company-helps-more-uw-women-break-into-tech-field/' target='_blank' rel='noopener noreferrer'>TUNE House Scholar - How TUNE House is advocating for diversity in tech</a></li>
           <li><a href='https://eng.uber.com/sre-internship/' target='_blank' rel='noopener noreferrer'>Uber Engineering Blog Article - My Site Reliability Engineering Internship Experience with Uber</a></li>
 	      	<li><a href=' https://www.facebook.com/uwswe/photos/a.548119975288589.1073741828.548103065290280/763141937119724/?type=3&theater' target='_blank' rel='noopener noreferrer'>UW SWE Member Feature - Why SWE has been transformational to my Husky Experience</a></li>
-	      	<li><a href='http://www.dailyuw.com/search/?sd=desc&l=25&sort=relevance&f=html&t=article%2Cvideo%2Cyoutube%2Ccollection&app=editorial&nsa=eedition&q=mitali' target='_blank' rel='noopener noreferrer'>UW Daily Articles - a collection of the articles that I have written for the UW Daily</a></li>
+	      	<li><a href='http://www.dailyuw.com/search/?f=html&q=mitali+palekar&sd=desc&l=25&t=article%2Ccollection%2Cvideo%2Cyoutube&nsa=eedition' target='_blank' rel='noopener noreferrer'>UW Daily Articles - a collection of the articles that I have written for the UW Daily</a></li>
         </ul>
       </div>
     );
