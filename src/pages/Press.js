72b730de20a2d734544367f7f6307b77d584ea65
Mitali Palekar <mitali97@cs.washington.edu>
2018-08-26 21:11:46 -0700

    New website

---
+++ src/pages/Press.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default class Press extends React.Component {
+  render() {
+    return (
+      <div id='press'>
+        <div id='title'>
+          <span>PRESS</span>
+        </div>
+        <p>
+          Over the last few years, I have been featured in a few articles as well as
+          written a few articles myself. If you have any questions and/or would like to
+          reach out to me regarding those articles, feel free to contact me below via
+          email, linkedin or twitter (in decreasing order of preference)!
+        </p>
+        <ul>
+          <li><a href='https://news.cs.washington.edu/2017/01/27/how-undergraduate-mitali-palekar-developed-a-passion-for-programming-and-diversity-at-uw-cse/' target='_blank' rel='noopener noreferrer'>UW CSE Student Spotlight - How programming and diversity is important to me</a></li>
+          <li><a href='https://www.tune.com/blog/2017-18-tune-house-scholars/' target='_blank' rel='noopener noreferrer'>TUNE House Scholar - 2017-18 Introductions</a></li>
+          <li><a href='http://q13fox.com/2017/05/12/with-unique-scholarship-program-seattle-company-helps-more-uw-women-break-into-tech-field/' target='_blank' rel='noopener noreferrer'>TUNE House Scholar - How TUNE House is advocating for diversity in tech [Q13 Fox]</a></li>
+          <li><a href='https://eng.uber.com/sre-internship/' target='_blank' rel='noopener noreferrer'>Uber Engineering Blog Article - My Site Reliability Engineering Internship Experience</a></li>
+          <li><a href='https://eng.uber.com/sre-internship/' target='_blank' rel='noopener noreferrer'>SWE Blog - Advice to High School Students</a></li>
+          <li><a href='https://eng.uber.com/sre-internship/' target='_blank' rel='noopener noreferrer'>UW SWE - Leading a 600+ member student organization</a></li>
+          <li><a href='https://www.facebook.com/uwswe/photos/a.548119975288589.1073741828.548103065290280/763141937119724/?type=3&theater' target='_blank' rel='noopener noreferrer'>UW SWE Member Feature - Why SWE has been transformational to my Husky Experience</a></li>
+          <li><a href='https://reigningit.wixsite.com/reigningit/testimonials' target='_blank' rel='noopener noreferrer'>Reigning It - Testimonial by Me!</a></li>
+          <li><a href='http://www.dailyuw.com/search/?f=html&q=mitali+palekar&sd=desc&l=25&t=article%2Ccollection%2Cvideo%2Cyoutube&nsa=eedition' target='_blank' rel='noopener noreferrer'>UW Daily Articles - a collection of the articles that I have written for the UW Daily</a></li>
+        </ul>
+      </div>
+    );
+  }
+}
---
+++ src/pages/Work.js
@@ -0,0 +1,98 @@
+import React from 'react'
+
+export default class Work extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      positions: [
+        {
+          photo: './photos/facebook.png',
+          photoAlt: 'Intern at Facebook',
+          title: 'Software Engineering Intern',
+          company: 'Facebook App Monetization',
+          description: 'Project TBD (Fall 2019)',
+          technologies: ''
+        },
+        {
+          photo: './photos/linkedin.jpg',
+          photoAlt: 'Intern at LinkedIn',
+          title: 'Software Engineering Intern',
+          company: 'LinkedIn Learning (Summer 2018)',
+          description: 'Architected and implemented a data-driven approach to access control for LinkedIn Learning, improving access control accuracy, observability, and scalability.',
+          technologies: 'Technologies: Java, Parseq, Pegasus Data Schemas, Rest.Li, XACML Access Control'
+        },
+        {
+          photo: './photos/stripe.jpg',
+          photoAlt: 'Intern at Stripe',
+          title: 'Software Engineering Intern',
+          company: 'Stripe, Search and Sigma (Spring 2018)',
+          description: 'Developed pipeline to automate creation of elastic search indices for large merchants, enabling searches for large merchants that would previously time out due to un-optimized data stores.',
+          technologies: 'Technologies: Ruby, ElasticSearch'
+        },
+        {
+          photo: './photos/uber.jpeg',
+          photoAlt: 'Intern at Uber',
+          title: 'Software Engineering Intern (Site Reliability)',
+          company: 'Uber, Platform Infrastructure  (Summer 2017)',
+          description: 'Architected and implemented in-production rack distribution analysis support allowing a rack aware distribution of service instances and preventing services from violating their service-level agreements, improving service reliability.',
+          technologies: 'Technologies: Golang, Mesos, Aurora, Uber’s Internal Tooling (service store, infrastructure store, indexing service)'
+        },
+        {
+          photo: './photos/seclab.png',
+          photoAlt: 'Research Assistant at UW CSE Security & Privacy Lab',
+          title: 'Undergraduate Research Assistant',
+          company: 'UW CSE Security and Privacy Lab',
+          description: 'Developing and maintaining Confidante, an encrypted email client using Keybase for automatic key management; implemented new features such as encrypted drafts, the user interface to allow for interactions with email threads when composing an email and optional private key signing',
+          technologies: 'Technologies: JavaScript, React, Flux, Node.js'
+        },
+        {
+          photo: './photos/daily.jpg',
+          photoAlt: 'Web Editor at Daily',
+          title: 'Web Editor',
+          company: 'The Daily - UW Newspaper',
+          description: 'Responsible for managing our online presence - more details to come!',
+          technologies: 'Technologies: TBD'
+        },
+        {
+          photo: './photos/NASA.jpg',
+          photoAlt: 'Intern at NASA',
+          title: 'Software Engineering Intern',
+          company: 'NASA Ames, Human Systems Lab  (Summer 2016)',
+          description: 'Designed and developed a path-finding tool and in-procedure execution guide using the Microsoft HoloLens for safety procedures and experiment conduction on the International Space Station, possibly replacing the current manual procedure execution process',
+          technologies: 'Technologies: Unity, C#, HoloToolkit API, Node.js, Git'
+        },
+        {
+          photo: './photos/gdtc.jpg',
+          photoAlt: 'Research Assistant at Georgian Digital Text Collective',
+          title: 'Undergraduate Research Intern',
+          company: 'UW Georgian Digital Text Collective',
+          description: 'Developed PHP scripts to convert Georgian to XML and JavaScript scripts to transpose XML to XHTML; proposed UI design changes to the front-end and managed the social media for the research group',
+          technologies: 'Technologies: PHP, Javascript, Ajax, HTML, CSS'
+        }
+      ]
+    };
+  }
+
+  render() {
+    var positionsHTML = this.state.positions.map(function(position, i) {
+      return (
+        <div id='position' key={i}>
+          <img src={require(`${position.photo}`)} alt={position.photoAlt}/>
+          <div id='position-title'><b>{position.title}</b></div>
+          <div><em>{position.company}</em></div>
+          <p id='position-description'>{position.description}</p>
+          <p><em>{position.technologies}</em></p>
+        </div>
+      );
+    });
+
+      return (
+        <div>
+          <div id='title'>
+            <span>WORK EXPERIENCE</span>
+          </div>
+          {positionsHTML}
+        </div>
+      );
+  }
+}
