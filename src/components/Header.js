f832ac234d62279d3e9c755a0fb0adf92c5c2169
Mitali Palekar <mitali97@cs.washington.edu>
2018-12-28 17:48:57 -0800

    Home Page updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,51 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavItem } from 'react-bootstrap'
 
 export default class Header extends React.Component {
 	render() {
 		return(
-      <header>
+      <Navbar collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="/"><span className="navItem">MITALI PALEKAR</span></a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="/work">
+              <span className="navItem">WORK</span>
+            </NavItem>
+            <NavItem eventKey={2} href="/projects">
+              <span className="navItem">PROJECTS</span>
+            </NavItem>
+            <NavItem eventKey={3} href="/leadership">
+              <span className="navItem">LEADERSHIP</span>
+            </NavItem>
+            <NavItem eventKey={4} href="/press">
+              <span className="navItem">PRESS</span>
+            </NavItem>
+            <NavItem eventKey={5} href="http://mitalipalekar.weebly.com" target="_blank" rel="noopener noreferrer">
+              <span className="navItem">HONORS</span>
+            </NavItem>
+            <NavItem eventKey={6} href="https://hlcmitalipalekar.weebly.com/" target="_blank" rel="noopener noreferrer">
+              <span className="navItem">HLC</span>
+            </NavItem>
+            <NavItem eventKey={7} href={require("../resume.pdf")} target="_blank" rel="noopener noreferrer">
+              <span className="navItem">RESUME</span>
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+	}
+}
+
+/*
+<header>
         <div className='container-fluid'>
-          <nav className='navbar-inverse navbar-default navbar-fixed-top' id='header-navbar'>
+          <nav className='navbar navbar-default navbar-fixed-top' id='header'>
             <div className='container-fluid'>
               <div className='navbar-header'>
                 <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1' aria-expanded='false'>
@@ -17,7 +56,7 @@ export default class Header extends React.Component {
                 </button>
                 <a className='navbar-brand' href='/'>MITALI PALEKAR</a>
               </div>
-              <div className='collapse navbar-collapse navbar-inverse' id='bs-example-navbar-collapse-1'>
+              <div className='collapse navbar-collapse'>
                 <ul className='nav navbar-nav navbar-right'>
                   <li><Link to='/'>HOME</Link></li>
                   <li><Link to='/work'>WORK</Link></li>
@@ -33,6 +72,4 @@ export default class Header extends React.Component {
           </nav>
         </div>
       </header>
-    );
-	}
-}
+*/
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,17 +8,17 @@ export default class Home extends React.Component {
           <img src={require("./photos/profile-photo.jpg")} alt="mitali" className="img-circle"/>
         </div>
         <div id='name'>
-          <span>MITALI PALEKAR</span>
+          <span><b>MITALI PALEKAR</b></span>
         </div>
         <div id='about'>
-          I am a senior pursuing <b>Computer Science</b> with <b>Interdiscplinary Honors</b> at
-          the <a href='https://www.washington.edu/' target='_blank' rel="noopener noreferrer">University of Washington</a>.
-          I am really interested in using technology to empower the voices of others and create products that benefit my community, 
-          especially through <b>machine learning</b>, <b>natural language processing</b> and <b>computer security</b>.  Over the last few years, I have 
-          been able to work towards this goal by interning at top Silicon Valley companies like <a>Facebook</a>, <a>LinkedIn</a>,
-          <a>Stripe</a>, <a href='https://eng.uber.com/sre-internship/' target='_blank' rel="noopener noreferrer">Uber</a> and  <a>NASA</a>. 
-          I am also an <b>Undergraduate Research Assistant</b> in the <a href='https://seclab.cs.washington.edu/' target='_blank' rel="noopener noreferrer">UW CSE Security 
-          & Privacy Lab</a> and the <b>Senior Adviser</b> (and past <b>President</b>) of the <a href='http://uwswe.com/' target='_blank' rel="noopener noreferrer">UW Society of Women Engineers</a>.  
+          I am a senior pursuing <b>Computer Science</b> with <b>Interdiscplinary Honors</b> at the  
+          <a href='https://www.washington.edu/' target='_blank' rel="noopener noreferrer"> University of Washington</a>.
+          I am interested in using technology to build products that benefit my community and empower the voices of others, 
+          through the lens of <b>machine learning</b>, <b>natural language processing</b> and <b>computer security</b>.  I have been fortunate 
+          to be able to work towards this goal by interning at many top Silicon Valley tech companies like <a><b>Facebook</b></a>, <a><b>LinkedIn</b></a>,
+          <a> <b>Stripe</b></a>, <a href='https://eng.uber.com/sre-internship/' target='_blank' rel="noopener noreferrer"><b>Uber</b></a> and  <a><b>NASA</b></a>. 
+          I am also an <b>Undergraduate Research Assistant</b> in the <a href='https://seclab.cs.washington.edu/' target='_blank' rel="noopener noreferrer">UW Security 
+          and Privacy Lab Resarch</a>, the <b>Senior Adviser</b> (and past <b>President</b>) of the <a href='http://uwswe.com/' target='_blank' rel="noopener noreferrer">UW Society of Women Engineers</a> and <b><a href="https://twitter.com/neo?lang=en" target='_blank' rel="noopener noreferrer">Neo</a> Scholar</b>.  
           In my free time, I love dancing, travelling, and gorging on guacamole.
         </div>
       </div>
